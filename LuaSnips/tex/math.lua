a43f983ce39afc93c318a007f0e985f3d25f8959
Dong W <wangder4@yeah.net>
2025-03-19 19:40:35 -0400

    regular update

--- LuaSnips/tex/math.lua
+++ LuaSnips/tex/math.lua
@@ -13,7 +13,73 @@ table.insert(snippets, s({
 ))
 
 table.insert(snippets, s({
-    trig = "([%u%l])-([01])-(%l)", desc = "Mathmode serial sequence",
+    trig = "frac", desc = "fraction",
+    snippetType = "autosnippet",
+    condition = in_mathzone,
+},
+    fmta("\\frac{<>}{<>}", { i(1), i(2)})
+))
+
+table.insert(snippets, s({
+    trig = "lim-", desc = "limit with no additionals",
+    snippetType = "autosnippet",
+    condition = in_mathzone,
+},
+    fmta("\\lim <>", { i(1), })
+))
+
+table.insert(snippets, s({
+    trig = "lim_", desc = "limit with lower scores",
+    snippetType = "autosnippet",
+    condition = in_mathzone,
+},
+    fmta("\\lim_{<>}", { i(1), })
+))
+
+table.insert(snippets, s({
+    trig = "lim([%u%l])", desc = "limit with X to infinity",
+    snippetType = "autosnippet",
+    condition = in_mathzone,
+    regTrig = true,
+    }, {
+        f(function (_, snip)
+            return string.format("\\lim_{%s \\to \\infty}", snip.captures[1])
+        end, {}),
+    }
+))
+
+table.insert(snippets, s({
+    trig = "int([-=%w])([-=%w])", desc = "integral flexible",
+    snippetType = "autosnippet",
+    condition = in_mathzone,
+    regTrig = true,
+    }, {
+        f(function (_, snip)
+            local opt1 = snip.captures[1]
+            local opt2 = snip.captures[2]
+            local output = "\\int"
+            if (not(opt1 == '-')) then
+                if (opt1 == '=') then
+                    output = output .. "_{-\\infty}"
+                else
+                    output = output .. string.format("_{%s}", opt1)
+                end
+            end
+
+            if (not(opt2 == '-')) then
+                if (opt2 == '=') then
+                    output = output .. "^{+\\infty}"
+                else
+                    output = output .. string.format("^{%s}", opt2)
+                end
+            end
+            return output
+        end, {}),
+    }
+))
+
+table.insert(snippets, s({
+    trig = "([%u%l])-([01])-([%u%l])", desc = "Mathmode serial sequence",
     snippetType = "autosnippet",
     condition = in_mathzone,
     regTrig = true,
