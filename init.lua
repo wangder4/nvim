2c773058d29025aaa4b914202f3a3b9c31ba9361
Dong W <wangder4@yeah.net>
2025-05-20 21:01:22 -0400

    Refine options, inspired by kickstarts.vim

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
--- Lua does not recognize my files. Include it to access functions in utils.lua
-package.path = package.path .. ';' .. vim.fn.stdpath('config') .. '/?.lua'
+-- -- Lua does not recognize my files. Include it to access functions in utils.lua
+-- package.path = package.path .. ';' .. vim.fn.stdpath('config') .. '/?.lua'
 local utils = require("utils")
 
-require("keymaps")      -- Non-plugin mapping
-require("clipboards")   -- Setup clipboards, WSL or OSC 52
-require("options")      -- Non-plugin options
+require("options")    -- Non-plugin options
+require("keymaps")    -- Non-plugin mapping
+require("clipboards") -- Setup clipboards, WSL or OSC 52
 
 -- Load plugin-free autocmds
 local autocmds_dir = '/lua/autocmds/'
@@ -19,21 +19,21 @@ local python3_host = ""
 local nodejs_host = ""
 
 if os.getenv('WSL_DISTRO_NAME') == "Debian" then
-    python3_host = vim.fn.expand('~/.pyvenv/311/bin/python3')
-    nodejs_host  = vim.fn.expand('~/.nvm/versions/node/v20.11.1/lib/node_modules/neovim/bin/cli.js')
+	python3_host = vim.fn.expand('~/.pyvenv/311/bin/python3')
+	nodejs_host  = vim.fn.expand('~/.nvm/versions/node/v20.11.1/lib/node_modules/neovim/bin/cli.js')
 end
 
 local hosts = {
-    python3_host_prog = python3_host,
-    node_host_prog = nodejs_host
+	python3_host_prog = python3_host,
+	node_host_prog = nodejs_host
 }
 
 for host, host_path in pairs(hosts) do
-    utils.set_host_prog(host, host_path)
+	utils.set_host_prog(host, host_path)
 end
 
-require("bootstrap")    -- Lazy.vim startup, enable plugins
-require("tunings")      -- nvim options for plug-in co-operation
+require("bootstrap") -- Lazy.vim startup, enable plugins
+require("tunings")   -- nvim options for plug-in co-operation
 
 -- Setup cursor style, not working
 -- vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
--- lua/options.lua
+++ lua/options.lua
@@ -1,86 +1,64 @@
-local set = vim.opt
-
--- Compatibility options
-set.compatible = false
-set.magic = true
-set.laststatus = 3
-set.showmode = true
+-- General Options
 vim.g.mapleader = "\\"
-vim.g.maplocalleader = " "
-vim.g.editorconfig = true
--- vim.o.signcolumn = "yes"
-vim.o.signcolumn = "no"
-
--- Undo options
+vim.g.maplocalleader = "\\"
+vim.g.have_nerd_font = false
+vim.o.showmode = false -- Displayed in status line already
+vim.o.mouse = 'a'
 vim.o.undofile = true
 vim.o.undolevels = 10000
+vim.o.confirm = true
+vim.o.splitright = true
+vim.o.splitbelow = true
 
--- Visual options
-vim.o.inccommand = "split"
-set.foldcolumn = '1'
-set.number = true
-set.numberwidth = 3
-set.scrolloff = 5
-set.sidescrolloff = 3
-set.wrap = false
-set.termguicolors = true
-set.cursorline = true
-set.colorcolumn = '80'
-set.foldexpr = "v:lua.require'conform'.formatexpr()"
--- vim.o.list = true
--- vim.opt.listchars = {
---     tab = "▏ ",
---     trail = "·",
---     extends = "»",
---     precedes = "«",
--- }
--- set.previewheight=10
+-- UI setting
+vim.o.number = true
+vim.o.breakindent = true
+vim.o.signcolumn = "yes"
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+vim.o.sidescrolloff = 10
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- vim.opt.foldcolumn = 1
+vim.opt.colorcolumn = '80'
 
--- Edit options
--- use zg to add new words to local dictionary, use zw to remove it.
-set.spell = true
-set.spelllang = { "en", "cjk" }
+-- Tabs
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
 
--- Tab options
-set.shiftwidth = 4
-set.softtabstop = 4
-set.expandtab = true
+-- Searching
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
--- Filetype options
-vim.cmd('syntax enable')
-vim.cmd('filetype plugin indent on')
-
--- Folding options
-vim.o.foldenable = true
+-- Folding
 vim.o.foldlevel = 999
-vim.o.foldlevelstart = 99           -- Do not fold anything at first glance
-vim.cmd('set foldopen-=search')     -- Search don't open fold
-vim.wo.foldmethod = 'expr'
-vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
--- vim.wo.foldmethod = 'syntax'
+vim.o.foldlevelstart = 99       -- Do not fold anything at first glance
+vim.cmd('set foldopen-=search') -- Search don't open fold
 
--- Search options
-set.ignorecase = true
-set.smartcase = true
-set.incsearch = true
-set.hlsearch = true
-set.wrapscan = true
 
--- Command-line options
-set.wildmenu = true
-set.wildignore:append { '*.o', '*.a', '__pycache__'}
-set.wildignore:append { '*.pyc', 'node_modules' }
-set.wildmode = { 'longest:list' }
-set.suffixes = { '.o', '.dvi' , 'bbl'}
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
+vim.g.wildignore:append { '*.o', '*.a', '__pycache__' }
+vim.g.wildignore:append { '*.pyc', 'node_modules' }
+vim.g.wildmode = { 'longest:list' }
 
--- Window options
--- set.splitbelow = true
+-- Timer setting
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300 -- Decrease mapped sequence wait time
 
--- netrw options
+-- use zg to add new words to local dictionary, use zw to remove it.
+vim.o.spell = true
+vim.o.spelllang = { "en", "cjk" }
+
 vim.g.netrw_list_hide = [[.aux$,.o$,.fdb_latexmk$,.fls$,.synctex.gz$]]
 
--- Performance options
-set.confirm = true
-set.autoread = true
-set.lazyredraw = false
-vim.o.updatetime = 200
+-- local set = vim.opt
+--
+-- -- Visual options
+-- set.foldexpr = "v:lua.require'conform'.formatexpr()"
+--
+-- -- Folding options
+-- vim.wo.foldmethod = 'expr'
+-- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
