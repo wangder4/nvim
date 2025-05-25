c3baafe1b6449bb1b1453d9a832484c4f51fdaf8
Dong W <wangder4@yeah.net>
2025-05-25 16:53:03 -0400

    Change keymap to personal preferences

--- lua/plugins/decoration/neo-tree.lua
+++ lua/plugins/decoration/neo-tree.lua
@@ -8,13 +8,13 @@ return {
     },
     lazy = false,
     keys = {
-        { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+        { '\\p', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
     },
     opts = {
         filesystem = {
             window = {
                 mappings = {
-                    ['\\'] = 'close_window',
+                    ['\\p'] = 'close_window',
                 },
             },
         },
